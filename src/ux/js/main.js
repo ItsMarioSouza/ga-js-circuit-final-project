28d6df582fe6abca7f0a1e423b0f8a4841f05232
Mario Souza <mario@myriadweb.com>
2017-03-12 18:18:21 -0400

    Added jquery ui's datepicker, updated main.js to push to firebase db

--- src/ux/js/main.js
+++ src/ux/js/main.js
@@ -1,4 +1,74 @@
-// initialize the configuration of map
+/* ––––––––––––––––––––––––––––––––––––––––  
+    Google Firebase API
+–––––––––––––––––––––––––––––––––––––––– */
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDec5ugKPiakUyykCmhA1mOBOFnCxK1oJA",
+    authDomain: "js-circuits-final-project-1.firebaseapp.com",
+    databaseURL: "https://js-circuits-final-project-1.firebaseio.com",
+    storageBucket: "js-circuits-final-project-1.appspot.com",
+    messagingSenderId: "189849567083"
+};
+
+firebase.initializeApp(config);
+
+
+/* ––––––––––––––––––––––––––––––––––––––––  
+    Global Variables
+–––––––––––––––––––––––––––––––––––––––– */
+var database = firebase.database();
+
+var reservationData = {};
+
+
+/* ––––––––––––––––––––––––––––––––––––––––  
+    Get & Push Input Form Data
+–––––––––––––––––––––––––––––––––––––––– */
+$('.form').on('submit', function(event) {
+    event.preventDefault();
+
+    var userInputName = $(".form__name").val();
+
+    var userInputDate = $(".form__date").val();
+        /* Variables can't be global becasuse they would always be set to "" at page load.
+           need to define them when data is ebtered or when the form is submitted */
+
+    if (userInputName === "" || userInputDate === "") {
+        alert("Form Field(s) are Empty");
+    } else {
+        reservationData.name = userInputName;
+
+        reservationData.date = userInputDate;
+
+        // RESET FORM...
+            // Found this online only works wiht the [0] index, but why?
+        $(".form")[0].reset();
+
+        var reservationsReference = database.ref('reservations');
+
+        reservationsReference.push(reservationData);
+
+        console.log("Data Sumbitted: " + userInputName + " " + userInputDate);
+    }
+});
+
+
+
+
+
+
+/* ––––––––––––––––––––––––––––––––––––––––  
+    JQuery Datepicker
+–––––––––––––––––––––––––––––––––––––––– */
+$( function() {
+    $( "#datePicker" ).datepicker();
+});
+
+
+/* ––––––––––––––––––––––––––––––––––––––––  
+    Google Maps API
+–––––––––––––––––––––––––––––––––––––––– */
+// Initialize the configuration of map
 function initMap() {
     // create a new instance of a map
     // configure map with options object
@@ -15,3 +85,5 @@ function initMap() {
         title: 'Monks Cafe'
     });
 }
+
+
